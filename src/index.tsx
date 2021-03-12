e7f8906aa38ccb4dfcca6555623aaa0b7cd6c49c
rahimfirudunlu <rehim.firudunlu1997@gmail.com>
2021-03-12 16:43:56 +0400

    ready

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,6 @@ import '@styles/styles.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import {App} from '@components/app/app';
+import App from './App';
 
 ReactDom.render(<App />, document.getElementById('root'));
